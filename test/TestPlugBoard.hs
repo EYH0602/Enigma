a867b892800e8442041053de027fd59c5ebe996c
Yifeng He <yfhe@ucdavis.edu>
2024-03-08 16:08:58 -0800

    add Gen for Plugboard

--- test/TestPlugBoard.hs
+++ test/TestPlugBoard.hs
@@ -1,10 +1,14 @@
 module TestPlugBoard where
 
 import Common (letters)
-import Plugboard (Plugboard, connectPlugboard, newPlugboard)
+import Data.List (nub, sort)
+import Plugboard (Plugboard, connectPlugboard, connectPlugboard', newPlugboard)
 import Test.QuickCheck
-  ( Property,
+  ( Gen,
+    Property,
+    forAll,
     quickCheck,
+    shuffle,
     (==>),
   )
 
@@ -21,8 +25,30 @@ prop_NoSwapIdentity c =
   c `elem` letters
     ==> connectPlugboard (newPlugboard 0 "") c == Just c
 
+makePairs :: String -> [(Char, Char)]
+makePairs zs = zip xs ys
+  where
+    (xs, ys) = splitAt ((length zs + 1) `div` 2) zs
+
+-- generator for Plugboard configurations
+genPlugboard :: Gen (Maybe Plugboard)
+genPlugboard = do
+  shuffled <- shuffle letters
+  let pairs = take 20 $ makePairs shuffled -- Take first 10 pairs for example
+      pairString = concatMap (\(a, b) -> [a, b]) pairs
+  return $ newPlugboard (length pairs `div` 2) pairString
+
+-- checker for randomly generated Plugboard
+isValidPlugboard :: Maybe Plugboard -> Bool
+isValidPlugboard (Just pb) = all (\c -> connectPlugboard' pb c == c || connectPlugboard' pb (connectPlugboard' pb c) == c) letters
+isValidPlugboard Nothing = True
+
+prop_connectPlugboard :: Property
+prop_connectPlugboard = forAll genPlugboard isValidPlugboard
+
 testAll :: IO ()
 testAll = do
   putStrLn "Testing Plugboard ..."
   quickCheck prop_SwapIdentity
   quickCheck prop_NoSwapIdentity
+  quickCheck prop_connectPlugboard
